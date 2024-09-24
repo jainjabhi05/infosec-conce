f28eb8832a1f4af26572ac4ea29bfc76fdd63f8c
jainjabhi05 <jainjabhi05@gmail.com>
2024-09-24 23:20:46 +0530

    static and package files added

---
+++ next.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/uZM6vxqHDq",
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
